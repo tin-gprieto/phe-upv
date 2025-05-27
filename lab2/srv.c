4cfcb77df1435557fac732b3d593afa00ffc87ab
tin-gprieto <mgonpri@upv.edu.es>
2025-05-27 21:47:34 +0200

    Fix srv in lab2

--- lab2/srv.c
+++ lab2/srv.c
@@ -7,48 +7,47 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define PORT 9999
+#define PORT 9999 
 
-#define DNI 2086546955761158
-#define BUFSIZE (64 + ( DNI % 23) )  
+unsigned char global [1024];
  
-void vulnerable(int client_fd, int client_id) {
-    	char buffer [BUFSIZE];	
-	printf ( "Se atiende al cliente #%d \n", client_id);
-	read(client_fd, buffer, 300); 
-	printf("Se recibe como payload: %s \n", buffer);
+void vulnerable( unsigned char * msg , int len ) {
+    	char buffer [64];	
+	memcpy(buffer, msg, len);
 }
- 
+
+void handle_client(int fd, int id){
+    ssize_t r = read(fd , global , sizeof(global) ) ;
+    printf ( "Se atiende al cliente #%d \n", id);
+    vulnerable(global,r) ;
+    close(fd) ;
+    puts( " Conexión cerrada. " ) ;
+}
+
+
 int main() {
-    int sockfd, client_fd;
-    struct sockaddr_in serv_addr, cli_addr;
-    socklen_t cli_len = sizeof(cli_addr);
- 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT);
- 
-    bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-    listen(sockfd, 5);
-    
-    printf ( " ==== Server in port %d ==== \n" , PORT) ;
-    int client_id = 0;
-    
-    while (1) {
-        client_fd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_len);
-    	client_id++;
-        if (fork() == 0) {
-            // Proceso hijo
-            close(sockfd);
-            vulnerable(client_fd, client_id);
-            exit(0);
-        } else {
-            // Padre
-            close(client_fd);
-            wait(NULL);
-        }
-    }
- 
-    return 0;
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in addr = {.sin_family = AF_INET ,
+				   .sin_port = htons ( PORT ) ,
+				   .sin_addr.s_addr = INADDR_ANY };
+	bind(sockfd , ( struct sockaddr *) & addr , sizeof(addr) ) ;
+	listen(sockfd , 1) ;
+
+	printf ( " ==== Server in port %d ==== \n" , PORT) ;
+	int client_id = 0;
+
+	while (1) {
+		int client_fd = accept(sockfd, NULL, NULL);
+		client_id++;
+		if ( !fork() ) {
+		    // Proceso hijo
+		    close(sockfd);
+		    handle_client(client_fd, client_id);
+		    exit(0);
+		}
+		close(client_fd) ;
+		// padre
+	}
+
+	return 0;
 }
